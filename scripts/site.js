9a7c137623984b6c07cc3a66b71f5e67d8abad00
Pho3nyxX <60277772+Pho3nyxX@users.noreply.github.com>
2025-11-21 14:39:59 -0500

    content is being changed

---
+++ scripts/site.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+	const btn = document.getElementById("updateUIBtn");
+	const txt = document.querySelector(".content input[type=text]");
+	const span = document.getElementById("updateSpan");
+
+	btn.addEventListener("click", (event) => {
+		let inputVal = txt.value;
+		if(inputVal === "" || inputVal === null){
+            return;
+        }else{
+            span.innerHTML = inputVal;
+        }
+	});
+});
